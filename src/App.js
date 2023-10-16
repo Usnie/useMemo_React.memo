da03f9be9a73495ef51b873a6e155e661bd70212
Usnie <Usnie.91@gmail.com>
2023-10-16 21:48:04 +0300

    Custom hook useLocalStorage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React from 'react';
-import Count from './components/Count';
-import IsFive from './components/IsFive';
+import { books } from "./data";
+import { Books } from "./components/Books";
+
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
-  const [count1, setCount1] = React.useState(0);
-  const [count2, setCount2] = React.useState(0);
+  const [order, setOrder] = useLocalStorage([], "order");
 
-  return (
-    <div className="App">
-      
-      <h5>Счетчик 1: </h5>
-      <div className="counter">
-        <button onClick={() => setCount1(count1 + 1)}>+</button>
-        <Count id={1} value={count1} />
-      </div>
+  const addToOrder = (id) => {
+    const newItem = books.find((item) => item.id === id);
 
-      <h5>Счетчик 2: </h5>
-      <div className="counter">
-        <button onClick={() => setCount2(count2 + 1)}>+</button>
-        <Count id={2} value={count2} />
-        <IsFive value={count2} />
-      </div>
-    </div>
-  );
+    setOrder([...order, newItem]);
+  };
+
+  return <Books items={books} addToOrder={addToOrder} />;
 }
 
 export default App;
